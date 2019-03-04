c28b8926936592c60252494dccb4ee6dc0306524
yang <nerosoft@outlook.com>
2019-03-05 00:30:19 +0800

    op comment

--- CPU.cpp
+++ CPU.cpp
@@ -243,6 +243,64 @@ return 0;
     }
 
 
+   /*** 
+    * ADC : ADD EITH CARRY
+    * SBC : SUBTRACT WITH CARRY
+    * AND : LOGICAL AND
+    * EOR : EXCLUSIVE OR
+    * ORA : LOGICAL INCLUSIVE OR
+    * ASL : ARITHMETIC SHIFT LEFT
+    * LSR : LOGICAL SHIFT RIGHT
+    * ROL : ROTATE LEFT
+    * ROR : ROTATE RIGHT
+    * BCC : BRANCH IF CARRY CLEAR
+    * BCS : BRANCH IF CARRY SET
+    * BEQ : BRANCH IF EQUAL
+    * BNE : BRANCH IF NOT EQUAL
+    * BIT : BIT TEST
+    * BMI : BRANCH IF MINUS
+    * BPL : BRANCH IF POSITIVE
+    * BRK : FORCE INTERRUPT
+    * BVC : BRANCH IF OVERFLOW CLEAR
+    * BVS : BRANCH IF OVERFLOW SET
+    * CLC : CLEAR CARRY FLAG
+    * SEC : SET CARRY FLAG
+    * CLD : CLEAR DECIMAL MODE
+    * SED : SET DECIMAL MODE
+    * CLR : CLEAR INTERRUPT DISABLE
+    * SEI : SET INTERRUPT DISABLE
+    * CLV : CLEAR OVERFLOW FLAG
+    * CMP : COMPARE
+    * CPX : COMPARE X REGISTER
+    * CPY : COMPARE Y REGISTER
+    * DEC : DECREMENT MEMORY
+    * DEX : DECREMENT X REGISTER
+    * DEY : DECREMENT Y REGISTER
+    * INC : INCREMENT MEMORY
+    * INX : INCREMENT X REGISTER
+    * INY : INCREMENT Y REGISTER
+    * JMP : JUMP
+    * JSR : JUMP TO SUBROUTINE
+    * RTS : RETURN FROM SUBROUTINE
+    * LDA : LOAD ACCUMULATOR
+    * LDX : LOAD X REGISTER
+    * LDY : LOAD Y REGISTER
+    * NOP : NO OPERATION
+    * PHA : PUSH ACCUMULATOR
+    * PLA : PULL ACCUMLATOR
+    * PHP : PUSH PROCESSOT STATUS
+    * PLP : PULL PROCESSOR STATUS
+    * RTI : RETURN FROM INTERRUPT
+    * STA : STORE ACCUMULATOR
+    * STX : STORE X REGISTER
+    * STY : STORE Y REGISTER
+    * TAX : TRANSFER ACCUMULATOR TO X
+    * TXA : TRANSFER X TO ACCUMULATOR
+    * TYA : TRANSFER Y TO ACCUMULATOR
+    * TAY : TRANSFER ACCUMULATOR TO Y
+    * TSX : TRANSFER STACK POINTER TO X
+    * TXS : TRANSFER  X TO STACK POINTER
+    **/ 
     void CPU::initOpHandlerTable(){
         // init ophandler map
         this->opHandlerTable.insert(std::pair<mos6502::i16,opHandler>(0x69,&CPU::ADC));
@@ -524,129 +582,7 @@ return 0;
 
     mos6502::i16 CPU::decode(){
         mos6502::i16 op = this->memory[this->PC];
-
-        /*
-        switch (op)
-        {
-            
-            // ADC : ADD EITH CARRY
-            case 0x69: case 0x64: case 0x75: case 0x6D: case 0x7D: case 0x79: case 0x61: case 0x71:
-            // SBC : SUBTRACT WITH CARRY
-            case 0xE9: case 0xE5: case 0xF5: case 0xED: case 0xFD: case 0xF9: case 0xE1: case 0xF1: 
-            // AND : LOGICAL AND
-            case 0x29: case 0x25: case 0x35: case 0x2D: case 0x3D: case 0x39: case 0x21: case 0x31: 
-            // EOR : EXCLUSIVE OR
-            case 0x49: case 0x45: case 0x55: case 0x4D: case 0x5D: case 0x59: case 0x41: case 0x51:
-            // ORA : LOGICAL INCLUSIVE OR
-            case 0x09: case 0x05: case 0x15: case 0x0D: case 0x1D: case 0x19: case 0x01: case 0x11:
-            // ASL : ARITHMETIC SHIFT LEFT
-            case 0x0A: case 0x06: case 0x16: case 0x0E: case 0x1E:
-            // LSR : LOGICAL SHIFT RIGHT
-            case 0x4A: case 0x46: case 0x56: case 0x4E: case 0x5E:
-            // ROL : ROTATE LEFT
-            case 0x2A: case 0x26: case 0x36: case 0x2E: case 0x3E:
-            // ROR : ROTATE RIGHT
-            case 0x6A: case 0x66: case 0x76: case 0x6E: case 0x7E:
-            // BCC : BRANCH IF CARRY CLEAR
-            case 0x90:
-            // BCS : BRANCH IF CARRY SET
-            case 0xB0:
-            // BEQ : BRANCH IF EQUAL
-            case 0xF0:
-            // BNE : BRANCH IF NOT EQUAL
-            case 0xD0:
-            // BIT : BIT TEST
-            case 0x24:
-            case 0x2C:
-            // BMI : BRANCH IF MINUS
-            case 0x30:
-            // BPL : BRANCH IF POSITIVE
-            case 0x10:
-            // BRK : FORCE INTERRUPT
-            case 0x00:
-            // BVC : BRANCH IF OVERFLOW CLEAR
-            case 0x50:
-            // BVS : BRANCH IF OVERFLOW SET
-            case 0x70:
-            // CLC : CLEAR CARRY FLAG
-            case 0x18:
-            // SEC : SET CARRY FLAG
-            case 0x38:
-            // CLD : CLEAR DECIMAL MODE
-            case 0xD8:
-            // SED : SET DECIMAL MODE
-            case 0xF8:
-            // CLR : CLEAR INTERRUPT DISABLE
-            case 0x58:
-            // SEI : SET INTERRUPT DISABLE
-            case 0x78:
-            // CLV : CLEAR OVERFLOW FLAG
-            case 0xB8:
-            // CMP : COMPARE
-            case 0xC9: case 0xC5: case 0xD5: case 0xCD: case 0xDD: case 0xD9: case 0xC1: case 0xD1:
-            // CPX : COMPARE X REGISTER
-            case 0xE0: case 0xE4: case 0xEC:
-            // CPY : COMPARE Y REGISTER
-            case 0xC0: case 0xC4: case 0xCC:
-            // DEC : DECREMENT MEMORY
-            case 0xC6: case 0xD6: case 0xCE: case 0xDE:
-            // DEX : DECREMENT X REGISTER
-            case 0xCA:
-            // DEY : DECREMENT Y REGISTER
-            case 0x88:
-            // INC : INCREMENT MEMORY
-            case 0xE6: case 0xF6: case 0xEE: case 0xFE:
-            // INX : INCREMENT X REGISTER
-            case 0xE8:
-            // INY : INCREMENT Y REGISTER
-            case 0xC8:
-            // JMP : JUMP
-            case 0x4C: case 0x6C:
-            // JSR : JUMP TO SUBROUTINE
-            case 0x20:
-            // RTS : RETURN FROM SUBROUTINE
-            case 0x60:
-            // LDA : LOAD ACCUMULATOR
-            case 0xA9: case 0xA5: case 0xB5: case 0xAD: case 0xBD: case 0xB9: case 0xA1: case 0xB1:
-            // LDX : LOAD X REGISTER
-            case 0xA2: case 0xA6: case 0xB6: case 0xAE: case 0xBE:
-            // LDY : LOAD Y REGISTER
-            case 0xA0: case 0xA4: case 0xB4: case 0xAC: case 0xBC:
-            // NOP : NO OPERATION
-            case 0xEA:
-            // PHA : PUSH ACCUMULATOR
-            case 0x48:
-            // PLA : PULL ACCUMLATOR
-            case 0x68:
-            // PHP : PUSH PROCESSOT STATUS
-            case 0x08:
-            // PLP : PULL PROCESSOR STATUS
-            case 0x28:
-            // RTI : RETURN FROM INTERRUPT
-            case 0x40:
-            // STA : STORE ACCUMULATOR
-            case 0x85: case 0x95: case 0x8D: case 0x9D: case 0x99: case 0x81: case 0x91:
-            // STX : STORE X REGISTER
-            case 0x86: case 0x96: case 0x8E:
-            // STY : STORE Y REGISTER
-            case 0x84: case 0x94: case 0x8C:
-            // TAX : TRANSFER ACCUMULATOR TO X
-            case 0xAA:
-            // TXA : TRANSFER X TO ACCUMULATOR
-            case 0x8A:
-            // TYA : TRANSFER Y TO ACCUMULATOR
-            case 0x98:
-            // TAY : TRANSFER ACCUMULATOR TO Y
-            case 0xA8:
-            // TSX : TRANSFER STACK POINTER TO X
-            case 0xBA:
-            // TXS : TRANSFER  X TO STACK POINTER
-            case 0x9A:
-
-            default:
-                break;
-
-        }*/
+        
         mos6502::i16 (CPU::*opHdr)(mos6502::i16) = this->opHandlerTable[op];
         
         return (this->*opHdr)(op);
