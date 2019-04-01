97fea9457c1a2c6dd2b91b09d47fdc7a26fd4f79
yang <nerosoft@outlook.com>
2019-04-01 22:41:46 +0800

    tile int8 add

--- src/PPU.cpp
+++ src/PPU.cpp
@@ -13,7 +13,14 @@ namespace ppu{
 
 
     mos6502::i8 *PPU::addTileInt8(mos6502::i8 first,mos6502::i8 second){
-        
+        mos6502::i8 result[8];
+        for(int i = 7; i>0; i--){
+            result[7-i] = (((first >> i) == 0x0) && ((second >> i) == 0x0)) ? 0x0 :
+                (((first >> i) == 0x1) && ((second >> i) == 0x1)) ? 0x3 :
+                (((first >> i) == 0x1) && ((second >> i) != 0x1)) ? 0x1 : 0x2;
+        }
+
+        return result;
     } 
 
 
